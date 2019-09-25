e24df81e2d69f0f3eecc023dec103668bc6668ad
luongquytnvn <luongquytnvn@gmail.com>
2019-09-25 11:50:52 +0700

    Bài tập sử dụng toán tử

---
+++ script.js
@@ -0,0 +1,7 @@
+let inputHeight;
+inputWidth = prompt("Nhập Width:")
+inputHeight = prompt('Nhập Height')
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+let area = Width * Height;
+document.write('area ='+ area);
---
+++ script2.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhập Width:")
+inputHeight = prompt('Nhập Height')
+//let Width = parseInt(inputWidth);
+//let Height = parseInt(inputHeight);
+let area = inputWidth * inputHeight;
+document.write('area ='+ area);
